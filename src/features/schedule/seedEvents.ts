6f827efa3be8d177b7a698019884d64018d663b8
yukihayashi <yukiha617@gmail.com>
2026-03-17 23:47:16 +0900

    イベントデータのソースを変更し、日本語表示に修正

--- src/features/schedule/seedEvents.ts
+++ src/features/schedule/seedEvents.ts
@@ -1,7 +1,7 @@
 import type {SeedEvent} from './types';
 import {parseTimeToMinutes} from './timeUtils';
 
-const seedResponse = require('../../../ai/002_seed_events_response.json') as {events: SeedEvent[]};
+const seedResponse = require('../../../mock/events_response.json') as {events: SeedEvent[]};
 
 export const seedEvents: SeedEvent[] = [...seedResponse.events].sort((left, right) => {
     return parseTimeToMinutes(left.f_time) - parseTimeToMinutes(right.f_time);
